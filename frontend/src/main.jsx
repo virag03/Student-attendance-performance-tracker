3fe4d5f299a43302049a1477f9ee40d1147f9eb2
Virag Hote <viraghote2020@gmail.com>
2025-11-09 17:30:07 +0530

    Fix merge conflict in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -22,4 +10,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
->>>>>>> master
